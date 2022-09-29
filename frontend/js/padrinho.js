3ce1350f7806f5f0dc3274f671b1e67bb18e0b42
lucimurata92 <midori9213@gmail.com>
2022-09-29 08:50:51 -0300

    Tentando criar a tabela dinâmica

--- frontend/js/padrinho.js
+++ frontend/js/padrinho.js
@@ -1,64 +1,57 @@
 const nomePadrinho = document.getElementById('padrinho');
 const nomeOng = document.getElementById('ong');
 const totalApadrinhados = document.getElementById('total_apadrinhados');
-const totalApadrinhamento = document.getElementById('total_apadrinhamento');
+const valorDoado = document.getElementById('valor');
 const porcentagemMetas = document.getElementById('porcentagem_metas');
 const dataMovimentacao = document.getElementById('data_movimentacao');
 const nomePet = document.getElementById('nome_pet');
 const totalArrecadadoCampanha = document.getElementById('total_arrecadado');
+const dadosDaTabela = document.getElementById('dadosTabela');
 const card = document.getElementsByClassName('card');
 const valoresCards = document.getElementsByClassName('num');
 
-const url = 'http://localhost:8080/apadrinhamento';
+const url = `http://localhost:8080/apadrinhamento/${localStorage.getItem('apadrinhador_id')}`;
 const urlSomatoriaPessoa = `http://localhost:8080/somatoria/idpessoa/${localStorage.getItem('apadrinhador_id')}`;
 const urlApadrinhamento = `http://localhost:8080/apadrinhamento/${localStorage.getItem('apadrinhador_id')}`;
 
-// function consultarMetas() {
+function popularTabela() {
 
-//     const options = {
-//         method: "GET",
-//         mode: "cors",
-//         headers: {
-//             'content-type': 'application/json',
-//         }
-//     }
-//     fetch(url, options).then(response => {
-//         return response.json();
-//     })  
-//         .then(data => {
-//             popularTabela(data);
-//             console.log(data)
-//         })
+    const options = {
+        method: "GET",
+        mode: "cors",
+        headers: {
+            'content-type': 'application/json',
+        }
+    }
+    fetch(url, options).then(response => {
+        return response.json();
+    
+    })  
+        .then(data => {
+            console.log('oi')
+            popularTabela(data);
+        })
 
-//     function atribuirCampos(data) {
-//         document.getElementById('padrinho').value = apadrinhamento.padrinho;
-//         document.getElementById('ong').value = apadrinhamento.nome_ong;
-//         document.getElementById("total_apadrinhados").value =  apadrinhamento.totalApadrinhados;
-//         document.getElementById("total_apadrinhamento").value =  apadrinhamento.totalApadrinhamento;
-//         document.getElementById("porcentagem_metas").value =  apadrinhamento.porcentagemMetas;
-//         document.getElementById("data_movimentacao").value =  apadrinhamento.data_apadrinhamento;
-//         document.getElementById("nome_campanha").value =  apadrinhamento.nomeCampanha;
-//         document.getElementById("total_arrecadado").value =  apadrinhamento.totalArrecadadoCampanha;
-//     }
+    function popularTabela(data){
+        console.log(data)
 
-//     function popularTabela(data){
-//         console.log(data)
+        let html = ""
+        data.forEach(causa => {
+            html += `
+                <tr>
+                    <td>${causa.data_apadrinhamento.value}</td>
+                    <td>${causa.animal.nome_pet.value}</td>
+                    <td>${causa.ong.nome_ong.value}</td>
+                    <td>${causa.pessoa.valor.value}</td>
+                </tr> 
+            `
+        });
+        document.getElementById("dadosTabela").innerHTML = html;
+        window.location.href = "d1-padrinho.html";
+    }
+}
 
-//         let html = ""
-//         data.forEach(causa => {
-//             html += `
-//                 <tr>
-//                     <td>${causa.data_apadrinhamento}</td>
-//                     <td>${causa.nome_pet}</td>
-//                     <td>${causa.nome_ong}</td>
-//                     <td>${causa.totalArrecadadoCampanha}</td>
-//                 </tr> 
-//             `
-//         });
-//         document.getElementById("dadosTabela").innerHTML = html;
-//         window.location.href = "d1-padrinho.html";
-//     }
-// }
+popularTabela();
 
 function card_one() {
 
@@ -96,7 +89,6 @@ function card_two() {
 
     }).then((data) => {
             const animais = Object.keys(data).reduce(function (previous, animal) {
-                console.log(data[animal].value)
                 return previous + data[animal].value;
             }, 0);
 
