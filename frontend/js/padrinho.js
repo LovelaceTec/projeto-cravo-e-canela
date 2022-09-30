a9be9639837aac223bf6df976078844032272ebd
Isabella <isacammerer@gmail.com>
2022-09-29 21:25:30 -0300

    Inserção das telas html restantes

--- frontend/js/padrinho.js
+++ frontend/js/padrinho.js
@@ -1,22 +1,20 @@
 const nomePadrinho = document.getElementById('padrinho');
 const nomeOng = document.getElementById('ong');
 const totalApadrinhados = document.getElementById('total_apadrinhados');
-const corpo = document.getElementById('corpo');
-const card = document.getElementsByClassName('text-column');
+const valorDoado = document.getElementById('valor');
+const porcentagemMetas = document.getElementById('porcentagem_metas');
+const dataMovimentacao = document.getElementById('data_movimentacao');
+const nomePet = document.getElementById('nome_pet');
+const totalArrecadadoCampanha = document.getElementById('total_arrecadado');
+const dadosDaTabela = document.getElementById('dadosTabela');
+const card = document.getElementsByClassName('card');
 const valoresCards = document.getElementsByClassName('num');
 
 const url = `http://localhost:8080/apadrinhamento/${localStorage.getItem('apadrinhador_id')}`;
 const urlSomatoriaPessoa = `http://localhost:8080/somatoria/idpessoa/${localStorage.getItem('apadrinhador_id')}`;
 const urlApadrinhamento = `http://localhost:8080/apadrinhamento/${localStorage.getItem('apadrinhador_id')}`;
 
-window.onload = function(){
-   
-    trazerDados();
-    console.log('Onload disparado!');
-}
-
-function trazerDados() {
-    debugger;
+function popularTabela() {
 
     const options = {
         method: "GET",
@@ -25,59 +23,35 @@ function trazerDados() {
             'content-type': 'application/json',
         }
     }
-    fetch(url, options)
-        .then(response => {
-            return response.json();
+    fetch(url, options).then(response => {
+        return response.json();
+    
+    })  
+        .then(data => {
+            console.log('oi')
+            popularTabela(data);
+        })
 
-        })
-        .then(function (jsonData) {
-            console.log(jsonData);
-            addRows(jsonData);
-        })
-        .catch(function (e) {
-            console.log('Deu erro!');
+    function popularTabela(data){
+        console.log(data)
+
+        let html = ""
+        data.forEach(causa => {
+            html += `
+                <tr>
+                    <td>${causa.data_apadrinhamento.value}</td>
+                    <td>${causa.animal.nome_pet.value}</td>
+                    <td>${causa.ong.nome_ong.value}</td>
+                    <td>${causa.pessoa.valor.value}</td>
+                </tr> 
+            `
         });
-    
-    
-    function addRows(jsonData) {
-        debugger;
-        
-
-        const nomePet = jsonData.animal.nome_pet
-        const nomeOng = jsonData.ong.nome_ong
-        const dataMov = jsonData.data_apadrinhamento
-        const valor = jsonData.valor
-      
-        let col=[];
-        let cabecalho = ["Nome" , "Ong" , "Data" , "Valor"]
-        let valores = [nomePet, nomeOng, dataMov, valor];
-       
-        const table = document.createElement("table");
-        let tr = table.insertRow(-1);                   // table row.
-
-        for (let i = 0; i < cabecalho.length; i++) {
-            let th = document.createElement("th");      // table header.
-            th.innerHTML = cabecalho[i];
-            tr.appendChild(th);
-        }
-
-        tr = table.insertRow(-1);                   // table row.
-
-        for (let i = 0; i < valores.length; i++) {
-            let th = document.createElement("th");      // table header.
-            th.innerHTML = valores[i];
-            tr.appendChild(th);
-        }
-
-        const divShowData = document.getElementById('showData');
-        divShowData.innerHTML = "";
-        divShowData.appendChild(table);
-
-       
+        document.getElementById("dadosTabela").innerHTML = html;
+        window.location.href = "d1-padrinho.html";
     }
 }
 
-//trazerDados();
+popularTabela();
 
 function card_one() {
 
